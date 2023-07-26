6c6a2341705910c9c3664f14564cce0367610c22
Safahadidy <safahadidy@gmail.com>
2023-07-26 07:48:24 -0700

    c

--- main.c
+++ main.c
@@ -7,6 +7,8 @@ int main(int ac, char **av char **env)
 	size_t size = 0;
         int n_char = 0, status;
 	pid_t pid;
+	while (1)
+{
         write(1,"$ ",2);
         n_char = getline(&buffer, &size, stdin);
         if(n_char == -1){
@@ -15,6 +17,18 @@ int main(int ac, char **av char **env)
         }
         buffer[n_char - 1] ='\0';
 	char **tokens = splitter(buffer);
-	if (strcmp(tokens[0]
+	if (strcmp(tokens[0], "exit")==0)
+		exit (0);
+	pid = fork()
+		if (pid == 0)
+		{
+			cmd = get_command(tokens[0]);
+			if (cmd)
+				exceve(cmd, tokens, env);
+			printf("Command not found\n");
+			exit (0);
+		}
+		wait(&status);
+}
 return(0);
 }
